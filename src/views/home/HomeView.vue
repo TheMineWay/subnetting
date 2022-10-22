76656f11663eb1fa7b5a2aaf2013d8437f51b18f
Joel Campos <joelcamposoliva9000@gmail.com>
2022-10-22 13:25:09 +0200

    Removed trash

---
+++ src/views/home/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>Hello world!</p>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  components: {},
+})
+export default class HomeView extends Vue { }
+</script>
