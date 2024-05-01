c90e4a7a84f7918d07d90939a2f5f22ea0dab136
TheMineWay <joelcamposoliva9000@gmail.com>
2024-05-01 17:40:24 +0200

    Installed UI

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,14 @@
 import { Translation } from "@/types/i18n/translation.enum";
 import setupI18n from "@/i18n/setup.i18n";
 import { PageProps } from "@/types/router/page-props.type";
+import SubnettingTool from "../../components/tool/subnet/subnetting-tool";
 
 export default async function Home({ params: { locale } }: PageProps) {
   const { t } = await setupI18n(locale, [Translation.COMMON]);
-  return <main>{t("test")}</main>;
+  return (
+    <main>
+      {t("test")}
+      <SubnettingTool />
+    </main>
+  );
 }
