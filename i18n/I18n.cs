5d2b17a659c8a095e8e89e9dc5327684d992d464
Joel Campos <joelcamposoliva9000@gmail.com>
2024-05-02 21:17:10 +0200

    i18n working

--- i18n/I18n.cs
+++ i18n/I18n.cs
@@ -1,21 +1,26 @@
+using Newtonsoft.Json.Linq;
+
 class I18n
 {
     // Instantiator
-    public static async Task<I18n> Create(string baseUrl)
+    public static async Task<I18n> Create(string baseUrl, bool disableCache = true)
     {
         // Read local storage
         Language language = Language.es_ES;
 
         // Fetch JSONs
-        var jsonResponse = await new HttpClient().GetAsync($"{baseUrl}/i18n/locales/{GetLanguageCode(language)}.json");
+        var jsonResponse = await new HttpClient().GetAsync($"{baseUrl}/i18n/locales/{GetLanguageCode(language)}.json{(disableCache ? ("?c=" + DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()) : "")}");
 
-        return new I18n();
+        var text = await jsonResponse.Content.ReadAsStringAsync() ?? throw new Exception();
+        return new I18n(I18nTranslationFile.CreateFromString(text));
     }
 
     private readonly Language language;
-    private I18n(Language language = Language.es_ES)
+    private readonly I18nTranslationFile i18nTranslationFile;
+    private I18n(I18nTranslationFile translationFile, Language language = Language.es_ES)
     {
         this.language = language;
+        i18nTranslationFile = translationFile;
     }
 
     public Language GetLanguage() => language;
@@ -24,7 +29,7 @@ class I18n
 
     public string T(string key)
     {
-        return key;
+        return i18nTranslationFile.GetValue(key);
     }
 
     // STATIC
@@ -37,6 +42,31 @@ class I18n
                 return "es-ES";
         }
         return "es-ES";
+    }
+}
+
+public class I18nTranslationFile
+{
+    public static I18nTranslationFile CreateFromString(string jsonString) => new I18nTranslationFile(JObject.Parse(jsonString));
+    private readonly JObject json;
+    public I18nTranslationFile(JObject json)
+    {
+        this.json = json;
+    }
+
+    public string GetValue(string route)
+    {
+        JToken? token = json;
+        string[] keys = route.Split('.');
+
+        foreach (var key in keys)
+        {
+            if (token?[key] == null) return route;
+
+            token = token[key];
+        }
+
+        return token?.ToString() ?? "";
     }
 }
 
