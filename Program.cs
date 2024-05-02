8280dcf15d8e7527f87eb9923ced262e3a0170af
Joel Campos <joelcamposoliva9000@gmail.com>
2024-05-02 20:36:54 +0200

    Inject I18n everywhere

--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+var i18n = await I18n.Create();
+
+builder.Services.AddSingleton(i18n);
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
---
+++ i18n/I18n.cs
@@ -0,0 +1,47 @@
+
+class I18n
+{
+    // Instantiator
+    public static async Task<I18n> Create()
+    {
+        // Read local storage
+        Language language = Language.es_ES;
+
+        // Fetch JSONs
+        var jsonResponse = await new HttpClient().GetAsync($"/i18n/locales/{GetLanguageCode(language)}.json");
+
+        return new I18n();
+    }
+
+    private readonly Language language;
+    private I18n(Language language = Language.es_ES)
+    {
+        this.language = language;
+    }
+
+    public Language GetLanguage() => language;
+
+    // Text
+
+    public string T(string key)
+    {
+        return key;
+    }
+
+    // STATIC
+
+    public static string GetLanguageCode(Language language)
+    {
+        switch (language)
+        {
+            case Language.es_ES:
+                return "es-ES";
+        }
+        return "es-ES";
+    }
+}
+
+enum Language
+{
+    es_ES
+}
